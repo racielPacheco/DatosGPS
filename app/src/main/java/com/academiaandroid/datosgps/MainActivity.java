b0f0438bf8d347310a42f156f9c0a68abf1d4b63
racielPacheco <racielpachecohernandez@gmail.com>
2019-09-09 17:15:11 -0500

    mejorado

--- app/src/main/java/com/academiaandroid/datosgps/MainActivity.java
+++ app/src/main/java/com/academiaandroid/datosgps/MainActivity.java
@@ -8,46 +8,44 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity extends AppCompat
 {
-    private TextView tvLatitud, tvLongitud, tvAltura, tvPrecision;
-
-    /*Se declara una variable de tipo LocationManager encargada de proporcionar acceso al servicio de localización del sistema.*/
-    private LocationManager locManager;
-    /*Se declara una variable de tipo Location que accederá a la última posición conocida proporcionada por el proveedor.*/
-    private Location loc;
+    LocationManager ubicacion;
+    TextView latitud, longitud;
+    Button actual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        latitud = (TextView) findViewById(R.id.latitud);
+        longitud = (TextView) findViewById(R.id.longitud);
+        actual=(Button) findViewById(R.id.actualizar);
+        actual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObtenerLocalizacion();
+            }
+        });
 
-        tvLatitud = (TextView)findViewById(R.id.tvLatitud);
-        tvLongitud = (TextView)findViewById(R.id.tvLongitud);
-        tvAltura = (TextView)findViewById(R.id.tvAltura);
-        tvPrecision = (TextView)findViewById(R.id.tvPrecision);
+    }
 
-        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    public void ObtenerLocalizacion() {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            tvLatitud.setText("No se han definido los permisos necesarios.");
-            tvLongitud.setText("");
-            tvAltura.setText("");
-            tvPrecision.setText("");
-
-            return;
-        }else
-        {
-            /*Se asigna a la clase LocationManager el servicio a nivel de sistema a partir del nombre.*/
-            locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            tvLatitud.setText(String.valueOf(loc.getLatitude()));
-            tvLongitud.setText(String.valueOf(loc.getLongitude()));
-            tvAltura.setText(String.valueOf(loc.getAltitude()));
-            tvPrecision.setText(String.valueOf(loc.getAccuracy()));
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },1000);
         }
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc = ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        latitud.setText("Latitud:  \n"+String.valueOf(loc.getLatitude()));
+        longitud.setText("Longitud:  \n"+String.valueOf(loc.getLongitude()));
     }
 }
